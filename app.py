92f1c0d5aa0aa1e80de9caadc66225350ad8c651
BenLittle24 <ben.little24@gmail.com>
2024-04-05 03:22:37 +0000

    First commit, db test added to app.py and updated requirements

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://db_lab10_user:J15PdkxYRXa2je7IaXRDJDGTKH8pza3u@dpg-co7mocf79t8c73emj0cg-a/db_lab10")
+    conn.close()
+    return "Database Connection Successful!"
